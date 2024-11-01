f36ff64988751a3f0182fbc262d60b530677de7c
Davi Gabriel <davigabrielmota123@gmail.com>
2024-11-01 18:45:08 -0300

    Update aulas

--- aula32.py
+++ aula32.py
@@ -4,13 +4,3 @@ None - Não valor
 is e is not = é não é (tipo, valor, identidade)
 id = Identidade
 """
-
-v1 = 'a'
-print(id(v1)) 
-
-# condicao = False
-
-# if condicao:
-#     print('Faça algo')
-# else:
-#     print('Não faça algo')
