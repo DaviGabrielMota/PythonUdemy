50d1409896eba9790db44c326909dc2c8ddc61e3
Davi Gabriel <davigabrielmota123@gmail.com>
2024-11-06 20:50:17 -0300

    update aulas

--- aula32.py
+++ aula32.py
@@ -4,3 +4,18 @@ None - Não valor
 is e is not = é não é (tipo, valor, identidade)
 id = Identidade
 """
+condicao = False
+passou_no_if = None
+
+
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    passou_no_if = None
+    print('Não faça nada')
+
+if passou_no_if is None:
+    print('Não passou no if')
+else:
+    print('Passou no if')
