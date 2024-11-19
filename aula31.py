95f82b0ef4d7480db60df03fb6f34097a0257ae1
Davi Gabriel <davigabrielmota123@gmail.com>
2024-11-19 12:47:54 -0300

    aulas

---
+++ aula31.py
@@ -0,0 +1,14 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+
+Imutáveis: str, int, float, bool
+
+"""
+
+string = 'davi Gabriel'
+outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# string[3] = 'ABC'
+# print(string)
+# print(outra_variavel)
+# print(string.capitalize())
+print(string.zfill(13))
