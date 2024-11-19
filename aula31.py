715230343a5d23bc565216a647d87d2e202af104
Davi Gabriel <davigabrielmota123@gmail.com>
2024-11-19 12:49:21 -0300

    Update

--- aula31.py
+++ aula31.py
@@ -6,27 +6,10 @@ Imutáveis: str, int, float, bool
 
 """
 
-string = 'davi Gabriel'
-outra_variavel = f'{string[:3]}ABC{string[4:]}'
-# string[3] = 'ABC'
-# print(string)
-# print(outra_variavel)
-# print(string.capitalize())
-print(string.zfill(13))
-=======
-Flag (Bandeira) - Marcar um local
-None - Não valor
-is e is not = é não é (tipo, valor, identidade)
-id = Identidade
-"""
-
-v1 = 'a'
-print(id(v1)) 
-
-# condicao = False
-
-# if condicao:
-#     print('Faça algo')
-# else:
-#     print('Não faça algo')
->>>>>>> 50d1409896eba9790db44c326909dc2c8ddc61e3
+# string = 'davi Gabriel'
+# outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# # string[3] = 'ABC'
+# # print(string)
+# # print(outra_variavel)
+# # print(string.capitalize())
+# print(string.zfill(13))
